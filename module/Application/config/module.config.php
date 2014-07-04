d70a1d88c58dbf848c6d8cfb037bdc00623aead1
kalmas <kylealmas@gmail.com>
2014-07-03 20:01:33 -0400

    just use __DIR__

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -6,12 +6,11 @@
  * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
-$config_dir = __DIR__;
 
 return array(
-    'router' => require $config_dir . '/router.config.php',
-    'service_manager' => require $config_dir . '/service_manager.config.php',
-    'translator' => require $config_dir . '/translator.config.php',
+    'router' => require __DIR__ . '/router.config.php',
+    'service_manager' => require __DIR__ . '/service_manager.config.php',
+    'translator' => require __DIR__ . '/translator.config.php',
     'controllers' => require __DIR__ . '/controllers.config.php',
     'view_manager' => require __DIR__ . '/view_manager.config.php'
 );
