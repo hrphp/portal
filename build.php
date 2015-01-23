fd687f59e3ff2b8693811590ace95073720120e1
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-01-22 23:20:02 -0500

    Optimized installation and build.

--- build.php
+++ build.php
@@ -2,13 +2,6 @@
 <?php
 chdir(__DIR__);
 
-fputs(STDOUT, "Running composer install\n");
-$returnStatus = null;
-passthru('composer install --dev', $returnStatus);
-if ($returnStatus !== 0) {
-    exit(1);
-}
-
 fputs(STDOUT, "Running PHP CodeSniffer\n");
 passthru('./vendor/bin/phpcs --standard=PSR2 --extensions=php module public', $returnStatus);
 if ($returnStatus !== 0) {
