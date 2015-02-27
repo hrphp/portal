2cacc71ea56178e5928b3ab905f86c63ad2f58c3
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-02-27 11:30:49 -0500

    Broke out "ago" algorithm.

--- module/Application/src/Application/Controller/Plugin/TwitterPlugin.php
+++ module/Application/src/Application/Controller/Plugin/TwitterPlugin.php
@@ -19,24 +19,7 @@ class TwitterPlugin extends AbstractPlugin
             $tweet = new \stdClass();
             $tweet->url = sprintf('https://twitter.com/hrphpmeetup/statuses/%d', $result->id);
             $tweet->excerpt = substr_replace($result->text, '...', 75);
-            $ago = round((time() - strtotime($result->created_at))/3600, 2);
-            if ($ago < .1) {
-                $tweet->ago = 'just now';
-            } elseif ($ago < .5) {
-                $tweet->ago = sprintf('%d minutes ago', round($ago*60));
-            } elseif ($ago < 1) {
-                $tweet->ago = 'less than an hour ago';
-            } elseif ($ago < 1.5) {
-                $tweet->ago = 'about an hour ago';
-            } elseif ($ago < 2.5) {
-                $tweet->ago = '2 hours ago';
-            } elseif ($ago < 24) {
-                $tweet->ago = sprintf('%d hours ago', $ago);
-            } elseif ($ago < 48) {
-                $tweet->ago = 'yesterday';
-            } else {
-                $tweet->ago = sprintf('%d days ago', round($ago/24));
-            }
+            $tweet->ago = $this->getTweetAgo($result->created_at);
             $tweets[] = $tweet;
         }
         return $tweets;
@@ -48,5 +31,36 @@ class TwitterPlugin extends AbstractPlugin
     public function getRawTweets()
     {
         return $this->getClient()->get('hrphpmeetup');
+    }
+
+    /**
+     * @param int $datetime
+     * @return string
+     */
+    private function getTweetAgo($datetime)
+    {
+        $ago = round((time() - strtotime($datetime))/3600, 2);
+        if ($ago < .1) {
+            return 'just now';
+        }
+        if ($ago < .5) {
+            return sprintf('%d minutes ago', round($ago*60));
+        }
+        if ($ago < 1) {
+            return 'less than an hour ago';
+        }
+        if ($ago < 1.5) {
+            return 'about an hour ago';
+        }
+        if ($ago < 2.5) {
+            return '2 hours ago';
+        }
+        if ($ago < 24) {
+            return sprintf('%d hours ago', $ago);
+        }
+        if ($ago < 48) {
+            return 'yesterday';
+        }
+        return sprintf('%d days ago', round($ago/24));
     }
 }
