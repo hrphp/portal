02748151785d953896cc182a27254688060b5a30
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-02-26 21:37:21 -0500

    Added documentation to the config files. Also trimmed some fat.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Global Configuration
+ *
+ * For simplicity's sake, all module settings exist here. In keeping with
+ * certain concepts pertaining to application configuration -- particularly
+ * those detailed at http://12factor.net/config -- this project defaults to the
+ * use of environment variables for storing sensitive information such as
+ * datastore credentials, API keys, etc. This practice makes the use of a local
+ * configuration file unnecessary; the use of a local configuration file is,
+ * however, supported by this project. See the local.php.dist file that ships
+ * with this project for more details about local configuration.
+ */
 
 return [
     'twitter' => [
